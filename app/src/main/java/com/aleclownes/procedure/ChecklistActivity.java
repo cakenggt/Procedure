6594c76a7e30e74d4226420814e4a901d5a273bc
Alec Lownes <alec.lownes@gmail.com>
2015-11-02 09:51:24 -0500

    Optimize imports in checklistActivity. Add tags to manager.

--- app/src/main/java/com/aleclownes/procedure/ChecklistActivity.java
+++ app/src/main/java/com/aleclownes/procedure/ChecklistActivity.java
@@ -6,7 +6,6 @@ import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
@@ -14,19 +13,12 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -36,10 +28,6 @@ public class ChecklistActivity extends AppCompatActivity {
     private static final String TAG = "ChecklistActivity";
     public static final String ID_KEY = "com.aleclownes.procedure.id";
     Checklist checklist;
-
-    public Checklist getChecklist(){
-        return checklist;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/aleclownes/procedure/ChecklistManager.java
+++ app/src/main/java/com/aleclownes/procedure/ChecklistManager.java
@@ -1,6 +1,8 @@
 package com.aleclownes.procedure;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -21,16 +23,20 @@ public interface ChecklistManager {
 
     public long create(Checklist checklist);
 
+    @Nullable
     public Checklist read(long id);
 
     public void update(Checklist checklist);
 
     public void delete(long id);
 
+    @NonNull
     public List<Checklist> getAllChecklists();
 
+    @NonNull
     public List<MasterChecklist> getAllMasterChecklists();
 
+    @NonNull
     public List<WorkingChecklist> getAllWorkingChecklists();
 
 }
